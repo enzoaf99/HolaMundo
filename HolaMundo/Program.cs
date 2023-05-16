1d60589fbdb495531a4c8ba8bc5e0ef56ffa80d8
enzoA <enzoA@ENZO>
2023-05-16 11:21:34 -0300

    Agregar archivos de proyecto.

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Introduce tu nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("Introduce tu apellido:");
+string apellido = Console.ReadLine();
+Console.WriteLine("Introduce tu edad:");
+string edad = Console.ReadLine();
+
+Console.WriteLine("Hola " + nombre + " " + apellido + ", tu edad es " + edad + " años");
+
+string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+Console.WriteLine(datetime);
